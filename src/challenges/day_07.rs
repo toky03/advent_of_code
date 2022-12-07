9bfb8ecbe762592192c229d955ddd66f360557fc
Jakob Marco <marco.jakob3@gmail.com>
2022-12-07 11:19:16 +0100

    test string parser

--- src/challenges/day_07.rs
+++ src/challenges/day_07.rs
@@ -1,74 +1,21 @@
 use std::collections::BTreeMap;
 use regex::Regex;
 
-#[derive(Clone, Debug)]
-struct File {
-    name: String,
-    size: u128,
-}
 
-impl File {
-    fn new(name: String, size: u128) -> Self {
-        File {
-            name,
-            size,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Directory<'a> {
-    directory_name: String,
-    parent_directory: Option<Box<&'a Directory<'a>>>,
-    files: Vec<File>,
-    sub_directories: Vec<Directory<'a>>,
-}
-
-impl Directory<'_> {
-
-    fn new<'a>(directory_name: String, parent_directory: Option<Box<&'a Directory<'a>>>) -> Directory<'a> {
-        Directory {
-            directory_name,
-            parent_directory,
-            files: Vec::new(),
-            sub_directories: Vec::new(),
-        }
-    }
-
-    fn add_file(&self, file_name: String, size: u128) -> Self {
-        let mut files = self.files.clone();
-        files.push(File::new(file_name, size));
-        Directory {
-            files,
-            sub_directories: self.sub_directories.to_vec(),
-            parent_directory: self.parent_directory.clone(),
-            directory_name: self.directory_name.clone(),
-        }
-    }
-
-    fn add_directory(&self, directory_name: String) -> Self {
-        let mut directories = self.sub_directories.clone();
-        directories.push(Directory::new(directory_name, Some(Box::new(self))));
-        Directory {
-            files: self.files.clone(),
-            sub_directories: directories,
-            parent_directory: self.parent_directory.clone(),
-            directory_name: self.directory_name.clone(),
-        }
-    }
-
-}
-
-fn parse_input(input_lines: Vec<&str>) -> Directory {
+fn parse_input(input_lines: Vec<&str>) -> String{
     let file_regex = Regex::new(r"(\d+) (\w+\.\w+)").unwrap();
-    let root_directory = Directory::new(String::from("/"), None);
-    input_lines.into_iter().fold(root_directory, |curr, command| {
-        if command.starts_with("dir ") {
-            return curr.add_directory(command.replace("dir ", "").to_string());
-        }
+    let change_directory_regex = Regex::new(r"& cd (\w+|/)").unwrap();
+    input_lines.into_iter().fold(String::from(""), |curr, command| {
         if file_regex.is_match(command) {
             let caps = file_regex.captures(command).unwrap();
-            return curr.add_file(caps.get(2).unwrap().as_str().to_string(), caps.get(1).unwrap().as_str().parse::<u128>().unwrap())
+            return format!("{curr}, {}", caps.get(1).unwrap().as_str());
+        }
+        if command == "& cd .." {
+            return  format!("{}\n", &curr[1..curr.len() - 3]);
+        }
+        if change_directory_regex.is_match(command) {
+            let caps = change_directory_regex.captures(command).unwrap();
+            return format!("\n{curr}-{}|", caps.get(1).unwrap().as_str());
         }
         curr
     })
@@ -77,7 +24,6 @@ fn parse_input(input_lines: Vec<&str>) -> Directory {
 
 #[cfg(test)]
 mod tests {
-    use super::Directory;
     use super::parse_input;
 
     #[test]
@@ -106,13 +52,23 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k";
+
         let splitted: Vec<&str> = input.lines().collect();
 
-        let result: Directory = parse_input(splitted);
-        let expected: Directory = Directory::new(String::from("/"), None);
+        let result = parse_input(splitted);
 
-        println!("{:?}", result);
+
+
+        let expected =
+            "/-|14848514,8504156
+/-a|29116,2557,62596
+/-a-e|584, i
+/
+/d|,4060174,8033020,5626152 d,7214296";
+
+
+        println!("{result}");
         
-        assert_eq!(result.directory_name, expected.directory_name)
+        assert_eq!(result, expected)
     }
 }
