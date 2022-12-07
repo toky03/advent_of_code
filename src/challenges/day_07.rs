067aa2ed95918bfc46bbbc25905ef72ae01a858e
Marco <marco.jakob3@gmail.com>
2022-12-07 13:37:25 +0100

    make debuggable

--- src/challenges/day_07.rs
+++ src/challenges/day_07.rs
@@ -1,26 +1,68 @@
-use std::collections::BTreeMap;
 use regex::Regex;
+use std::collections::BTreeMap;
 
-
-fn parse_input(input_lines: Vec<&str>) -> String{
-    let file_regex = Regex::new(r"(\d+) (\w+\.\w+)").unwrap();
-    let change_directory_regex = Regex::new(r"& cd (\w+|/)").unwrap();
-    input_lines.into_iter().fold(String::from(""), |curr, command| {
-        if file_regex.is_match(command) {
-            let caps = file_regex.captures(command).unwrap();
-            return format!("{curr}, {}", caps.get(1).unwrap().as_str());
-        }
-        if command == "& cd .." {
-            return  format!("{}\n", &curr[1..curr.len() - 3]);
-        }
-        if change_directory_regex.is_match(command) {
-            let caps = change_directory_regex.captures(command).unwrap();
-            return format!("\n{curr}-{}|", caps.get(1).unwrap().as_str());
-        }
-        curr
-    })
+struct Terminal {
+    accumulated_lines: String,
+    current_directory: String,
 }
 
+impl Terminal {
+    fn new() -> Self {
+        Terminal {
+            accumulated_lines: String::from("/"),
+            current_directory: String::from("/"),
+        }
+    }
+
+    fn change_up(&self) -> Terminal {
+        let current_directory = &self.current_directory.as_str()[0..(self.current_directory.len() - 2)];
+        Terminal {
+            accumulated_lines: format!("{}\n{}", self.accumulated_lines, self.current_directory.clone()),
+            current_directory: current_directory.to_string(),
+        }
+    }
+
+    fn add_file(&self, file_size: &str) -> Terminal {
+        let accumulated_lines = format!("{}{file_size},", self.accumulated_lines);
+        Terminal {
+            accumulated_lines,
+            current_directory: self.current_directory.clone(),
+        }
+    }
+
+    fn change_directory(&self, directory_name: &str) -> Terminal {
+        let current_directory = format!("{}{directory_name}/", self.current_directory.clone());
+        Terminal {
+            accumulated_lines: format!("{}\n{}", self.accumulated_lines, current_directory.clone()),
+            current_directory,
+        }
+    }
+}
+
+fn parse_input(input_lines: Vec<&str>) -> String {
+    let file_regex = Regex::new(r"(\d+) (\w+\.\w+)").unwrap();
+    let change_directory_regex = Regex::new(r"\$\s*cd\s*(\w+)").unwrap();
+    let initial_terminal = Terminal::new();
+    input_lines
+        .into_iter()
+        .fold(initial_terminal, |curr, command| {
+            if file_regex.is_match(command) {
+                let caps = file_regex.captures(command).unwrap();
+                let file_size = caps.get(1).unwrap().as_str();
+                return curr.add_file(file_size);
+            }
+            if command == "$ cd .." {
+                return curr.change_up();
+            }
+            if change_directory_regex.is_match(command) {
+                let caps = change_directory_regex.captures(command).unwrap();
+                let directory = caps.get(1).unwrap().as_str();
+                return curr.change_directory(directory);
+            }
+            curr
+        })
+        .accumulated_lines
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,8 +70,7 @@ mod tests {
 
     #[test]
     fn test_parse_input() {
-        let input =
-            "$ cd /
+        let input = "$ cd /
 $ ls
 dir a
 14848514 b.txt
@@ -57,18 +98,14 @@ $ ls
 
         let result = parse_input(splitted);
 
-
-
-        let expected =
-            "/-|14848514,8504156
-/-a|29116,2557,62596
-/-a-e|584, i
+        let expected = "/14848514,8504156,
+/a/29116,2557,62596,
+/a/e/584,
 /
-/d|,4060174,8033020,5626152 d,7214296";
-
+/d/4060174,8033020,5626152 d,7214296,";
 
         println!("{result}");
-        
+
         assert_eq!(result, expected)
     }
 }
